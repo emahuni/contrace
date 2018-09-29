1eaeb999af1b5cdd95e8fa7b023cd36d422a048b
Emmanuel Mahuni <emahuni@gmail.com>
2018-09-29 16:28:13 +0200

    docs(examples): add basic example and screenshot

---
+++ examples/basic.js
@@ -0,0 +1,25 @@
+const contra = require('..')();
+
+const obj = {
+  foo: 'inonzi',
+  bar: {
+    wadii: 'hapana',
+  },
+  fn: function fanikisheni () {
+    // ... do stuff in here
+    let zvikazoti = 'this that what what';
+
+    return value;
+  },
+  thinga: 76234,
+  mabob: 0x847,
+  murayini: [
+    {a: 'ehe'},
+    2,
+    5,
+    'five',
+    'blah',
+  ],
+};
+
+contra.debug('this is the result of obj: ', obj);
